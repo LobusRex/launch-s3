4a86e021beea73d846fb7ccc1e966cbcc5f92d48
LobusRex <43432714+LobusRex@users.noreply.github.com>
2023-12-18 20:31:12 +0100

    Loosen coupling around expansion selection

--- LobuS3Launcher/ExpansionControl.xaml.cs
+++ LobuS3Launcher/ExpansionControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using Common;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace LobuS3Launcher.Tabs
@@ -8,6 +11,8 @@ namespace LobuS3Launcher.Tabs
 	/// </summary>
 	public partial class ExpansionControl : UserControl
 	{
+		private Expansion Expansion { get; }
+
 		public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(ExpansionControl));
 
 		public string Title
@@ -16,11 +21,79 @@ namespace LobuS3Launcher.Tabs
 			set { SetValue(TitleProperty, value); }
 		}
 
-		public ExpansionControl()
+		public ExpansionControl(Expansion expansion)
 		{
 			InitializeComponent();
 
 			DataContext = this;
+
+			Expansion = expansion;
+
+			checkBox.Checked += CheckBox_Checked;
+			checkBox.Unchecked += CheckBox_Unchecked;
+			discCombo.Selected += Disc_Selected;
+			steamCombo.Selected += Steam_Selected;
+		}
+
+		public void UpdateControls()
+		{
+			Expansion.Update();
+
+			List<ExpansionSource> sources = Expansion.Sources.ToList();
+
+			steamCombo.IsEnabled = sources.Contains(ExpansionSource.Steam);
+			discCombo.IsEnabled = sources.Contains(ExpansionSource.Disc);
+			IsEnabled = Expansion.IsInstalled;
+
+			SilentCheckUpdate();
+		}
+
+		private void CheckBox_Checked(object sender, RoutedEventArgs e)
+		{
+			Expansion.Select();
+
+			UpdateControls();
+		}
+
+		private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+		{
+			Expansion.Deselect();
+
+			UpdateControls();
+		}
+
+		private void Disc_Selected(object sender, RoutedEventArgs e)
+		{
+			Expansion.SetPreferredSource(ExpansionSource.Disc);
+
+			UpdateControls();
+		}
+
+		private void Steam_Selected(object sender, RoutedEventArgs e)
+		{
+			Expansion.SetPreferredSource(ExpansionSource.Steam);
+
+			UpdateControls();
+		}
+
+		private void SilentCheckUpdate()
+		{
+			// Update the CheckBox.
+			checkBox.Checked -= CheckBox_Checked;
+			checkBox.Unchecked -= CheckBox_Unchecked;
+			checkBox.IsChecked = Expansion.IsSelected;
+			checkBox.Checked += CheckBox_Checked;
+			checkBox.Unchecked += CheckBox_Unchecked;
+
+			// Update the Disc ComboBoxItem.
+			discCombo.Selected -= Disc_Selected;
+			discCombo.IsSelected = Expansion.PreferredSource == ExpansionSource.Disc;
+			discCombo.Selected += Disc_Selected;
+
+			// Update the Steam ComboBoxItem.
+			steamCombo.Selected -= Steam_Selected;
+			steamCombo.IsSelected = Expansion.PreferredSource == ExpansionSource.Steam;
+			steamCombo.Selected += Steam_Selected;
 		}
 	}
 }
--- LobuS3Launcher/ExpansionTab.xaml.cs
+++ LobuS3Launcher/ExpansionTab.xaml.cs
@@ -11,7 +11,7 @@ namespace LobuS3Launcher.Tabs
 	/// </summary>
 	public partial class ExpansionTab : UserControl
 	{
-		private List<ExpansionPack> ExpansionPacks { get; }
+		private List<ExpansionControl> ExpansionControls { get; }
 
 		public TabItem? TabItemActions { get; set; } = null;
 
@@ -19,34 +19,59 @@ namespace LobuS3Launcher.Tabs
 		{
 			InitializeComponent();
 
-			ExpansionPacks = new List<ExpansionPack>()
-			{
-				// Expansion Packs
-				new(EP01Expansion, "The Sims 3 World Adventures"),
-				new(EP02Expansion, "The Sims 3 Ambitions"),
-				new(EP03Expansion, "The Sims 3 Late Night"),
-				new(EP04Expansion, "The Sims 3 Generations"),
-				new(EP05Expansion, "The Sims 3 Pets"),
-				new(EP06Expansion, "The Sims 3 Showtime"),
-				new(EP07Expansion, "The Sims 3 Supernatural"),
-				new(EP08Expansion, "The Sims 3 Seasons"),
-				new(EP09Expansion, "The Sims 3 University Life"),
-				new(EP10Expansion, "The Sims 3 Island Paradise"),
-				new(EP11Expansion, "The Sims 3 Into The Future"),
+			ExpansionControls = new List<ExpansionControl>();
 
-				// Stuff Packs
-				new(SP01Expansion, "The Sims 3 High-End Loft Stuff"),
-				new(SP02Expansion, "The Sims 3 Fast Lane Stuff"),
-				new(SP03Expansion, "The Sims 3 Outdoor Living Stuff"),
-				new(SP04Expansion, "The Sims 3 Town Life Stuff"),
-				new(SP05Expansion, "The Sims 3 Master Suite Stuff"),
-				new(SP06Expansion, "The Sims 3 Katy Perry Sweet Treats"),
-				new(SP07Expansion, "The Sims 3 Diesel Stuff"),
-				new(SP08Expansion, "The Sims 3 70s 80s & 90s Stuff"),
-				new(SP09Expansion, "The Sims 3 Movie Stuff"),
+			// Expansion Packs.
+			ExpansionControl[] expansions = new[]
+			{
+				CreateExpansionControl("World Adventures",          "The Sims 3 World Adventures"),
+				CreateExpansionControl("Ambitions",                 "The Sims 3 Ambitions"),
+				CreateExpansionControl("Late Night",                "The Sims 3 Late Night"),
+				CreateExpansionControl("Titanfall",                 "The Sims 3 Titanfall"),
+				CreateExpansionControl("Generations",               "The Sims 3 Generations"),
+				CreateExpansionControl("Pets",                      "The Sims 3 Pets"),
+				CreateExpansionControl("Showtime",                  "The Sims 3 Showtime"),
+				CreateExpansionControl("Supernatural",              "The Sims 3 Supernatural"),
+				CreateExpansionControl("Seasons",                   "The Sims 3 Seasons"),
+				CreateExpansionControl("University Life",           "The Sims 3 University Life"),
+				CreateExpansionControl("Island Paradise",           "The Sims 3 Island Paradise"),
+				CreateExpansionControl("Into the Future",           "The Sims 3 Into The Future"),
 			};
 
+			// Stuff Packs.
+			ExpansionControl[] stuffs = new[]
+			{
+				CreateExpansionControl("High-End Loft Stuff",       "The Sims 3 High-End Loft Stuff"),
+				CreateExpansionControl("Fast Lane Stuff",           "The Sims 3 Fast Lane Stuff"),
+				CreateExpansionControl("Outdoor Living Stuff",      "The Sims 3 Outdoor Living Stuff"),
+				CreateExpansionControl("Town Life Stuff",           "The Sims 3 Town Life Stuff"),
+				CreateExpansionControl("Master Suite Stuff",        "The Sims 3 Master Suite Stuff"),
+				CreateExpansionControl("Katy Perry's Sweet Treats", "The Sims 3 Katy Perry Sweet Treats"),
+				CreateExpansionControl("Diesel Stuff",              "The Sims 3 Diesel Stuff"),
+				CreateExpansionControl("70s, 80s, &amp; 90s Stuff", "The Sims 3 70s 80s & 90s Stuff"),
+				CreateExpansionControl("Movie Stuff",               "The Sims 3 Movie Stuff"),
+			};
+
+			ExpansionControls.AddRange(expansions);
+			ExpansionControls.AddRange(stuffs);
+
+			// Add expansions to the window.
+			foreach (var expansion in expansions)
+				EPPanel.Children.Add(expansion);
+
+			foreach (var stuff in stuffs)
+				SPPanel.Children.Add(stuff);
+
 			Loaded += ExpansionTab_Loaded;
+		}
+
+		private static ExpansionControl CreateExpansionControl(string title, string gameKey)
+		{
+			return new ExpansionControl(new Expansion(gameKey))
+			{
+				Title = title,
+				Margin = new Thickness(0, 10, 0, 0),
+			};
 		}
 
 		private void ExpansionTab_Loaded(object sender, RoutedEventArgs e)
@@ -69,7 +94,7 @@ namespace LobuS3Launcher.Tabs
 			EPBox.Visibility = Visibility.Visible;
 			SPBox.Visibility = Visibility.Visible;
 
-			foreach (var expansion in ExpansionPacks)
+			foreach (var expansion in ExpansionControls)
 				expansion.UpdateControls();
 		}
 
