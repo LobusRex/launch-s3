2884f16769f038cb35ec3ff5af122ecf7ca4bfe9
LobusRex <43432714+LobusRex@users.noreply.github.com>
2026-04-15 00:16:35 +0200

    Fix an issue where many click events were added to the same buttons. (#6)

--- source/LobuS3Launcher/ModsTab.xaml.cs
+++ source/LobuS3Launcher/ModsTab.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Common;
+using Microsoft.Win32;
 using ModernWpf.Controls;
 using System;
 using System.Collections.Generic;
@@ -19,15 +20,18 @@ public partial class ModsTab : UserControl
 	{
 		InitializeComponent();
 
-		Loaded += ModsTab_Loaded;
+		Loaded += modsTab_Loaded;
 	}
 
-	private void ModsTab_Loaded(object sender, RoutedEventArgs e)
+	private void modsTab_Loaded(object sender, RoutedEventArgs e)
 	{
 		UpdateCheckBoxes();
 
-		addOverrideButton.Click += (sender, e) => AddModButton_Click(sender, e, ModManager.Overrides);
-		addPackageButton.Click += (sender, e) => AddModButton_Click(sender, e, ModManager.Packages);
+		addOverrideButton.Click -= addOverrideButton_Click;
+		addOverrideButton.Click += addOverrideButton_Click;
+
+		addPackageButton.Click -= addPackageButton_Click;
+		addPackageButton.Click += addPackageButton_Click;
 	}
 
 	private void UpdateCheckBoxes()
@@ -109,12 +113,24 @@ public partial class ModsTab : UserControl
 		// Update the tab.
 		UpdateCheckBoxes();
 	}
-
-	private void AddModButton_Click(object sender, RoutedEventArgs e, ModSelector modSelector)
+	
+	private void addPackageButton_Click(object sender, RoutedEventArgs e)
 	{
-		var dialog = new Microsoft.Win32.OpenFileDialog();
-		dialog.DefaultExt = ".package";
-		dialog.Filter = "Package mods |*.package";
+		pickAndAddModTo(ModManager.Packages);
+	}
+
+	private void addOverrideButton_Click(object sender, RoutedEventArgs e)
+	{
+		pickAndAddModTo(ModManager.Overrides);
+	}
+
+	private void pickAndAddModTo(ModSelector modSelector)
+	{
+		var dialog = new OpenFileDialog
+		{
+			DefaultExt = ".package",
+			Filter = "Package mods |*.package"
+		};
 
 		bool? result = dialog.ShowDialog();
 
