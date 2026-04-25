a108acc4dc31103a9c28262c4aa52984f535b3f8
LobusRex <43432714+LobusRex@users.noreply.github.com>
2026-04-25 13:25:20 +0200

    Move hardcoded expansions to appsettings. (#8)
    * Move the hardcoded expansions to appsettings.
    
    * Add summary and copy appsettings during build.
    
    * Created addExpansions method.

---
+++ source/LobuS3Launcher/ExpansionConfiguration/ExpansionsSection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace LobuS3Launcher.ExpansionConfiguration;
+
+public class ExpansionsSection
+{
+	public static readonly string SectionName = "Expansions";
+
+	public List<ExpansionItem> ExpansionPacks { get; set; } = [];
+	public List<ExpansionItem> StuffPacks { get; set; } = [];
+}
